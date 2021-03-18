add6864e63cadd945c71f6c00c6bee616c1360df
K-20 <69999193+K-209@users.noreply.github.com>
2021-03-18 15:17:41 -0400

    Add files via upload

--- 9Ball.py
+++ 9Ball.py
@@ -27,19 +27,19 @@ async def status():
         await sleep(500)
         await bot.change_presence(activity=discord.Streaming(name="WonUpped Live!", url="http://www.twitch.tv/wonupped"))
         await sleep(500)
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game(""))
+        await bot.change_presence(activity=discord.Streaming(name="Microninjaguy Live!", url="http://www.twitch.tv/microninjaguy"))
         await sleep(500)
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game("GAWR"))
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game("/9clear"))
         await sleep(500)
         await bot.change_presence(status=discord.Status.online, activity=discord.Game("Gotta Catch 'em All"))
         await sleep(500)
         await bot.change_presence(status=discord.Status.online, activity=discord.Game("Ask me Question"))
         await sleep(500)
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game("SOMEbody once told me"))
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game("Bad Things!!"))
         await sleep(500)
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game("The world is gonna roll me"))
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game("Bonk!!"))
         await sleep(500)
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game("NineBall, your local questioner!"))
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game("NineBall, your local questioneer!"))
         await sleep(500)
         await bot.change_presence(status=discord.Status.online, activity=discord.Game("/9b"))
         await sleep(500)
@@ -95,16 +95,16 @@ async def _9ball(ctx, *, question):
                 "Oh hecc naw!",
                 "Definitely."]
 
-    colors = [0x680af5,0x3b21de,0x37c87e,0xe98316,0xee1115,0xed129f,0xed3212,0x1ecbe1,0xf6fff9,0xecbe1,]
-    embed=discord.Embed(title="The Extraordinaire 9Ball has Spoken.", color= random.choice(colors))
+    colors = [0x680af5,0x2E10ED,0x8CF9C1,0xF88000,0xFCFF00,0xed129f,0xed3212,0x1ACFE7,0x0FD150,0xFE2D00,]
+    embed=discord.Embed(title="The official 9Ball has Spoken.", color= random.choice(colors))
     embed.set_author(name="Asked by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
     embed.add_field(name="Question:", value=question, inline=False)
     embed.add_field(name="Answer:", value=random.choice(responses), inline=False)
-    embed.set_footer(text="Commands: /9b | /help")
+    embed.set_footer(text="Commands: /9b | /9help")
     await ctx.send(embed=embed)
     await ctx.message.delete()
 
-@bot.command()
+@bot.command(aliases=['9help', '9Help']) #The 9Ball help commands...
 async def _9help(ctx):
     embed=discord.Embed(title="9Ball Help Commands", color=0x680af5)
     embed.set_author(name="Requested by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
@@ -114,7 +114,7 @@ async def _9help(ctx):
     embed.add_field(name="/9about", value="The about page for 9Ball.", inline=True)
     await ctx.send(embed=embed)
 
-@bot.command()
+@bot.command(aliases=['9about', '9About']) #The about page for 9Ball
 async def _9about(ctx):
     embed=discord.Embed(title="Developed By K-20", description="Written in Python Rewrite. It's not an 8Ball but, It's a 9Ball!", color=0x680af5)
     embed.set_author(name="Requested by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
@@ -122,7 +122,7 @@ async def _9about(ctx):
     embed.add_field(name="Website:", value="https://sites.google.com/view/9-ball-bot/home", inline=False)
     await ctx.send(embed=embed)
 
-@bot.command()
+@bot.command(aliases=['9stats', '9stat']) #Devs stats for the current 9Ball
 async def _9stats(ctx):
     pythonVerison = platform.python_version()
     serverCount = len(bot.guilds)
@@ -134,7 +134,7 @@ async def _9stats(ctx):
     await ctx.send(embed=embed)
     await ctx.message.delete()
 
-@bot.command(aliases=['delete', 'purge', 'c', 'remove']) #Best clear command I've ever done
+@bot.command(aliases=['delete', 'purge', 'c', 'remove', '9clear']) #Best clear command I've ever done
 async def _9clear(ctx, amount: int):
     if ctx.message.author.guild_permissions.manage_messages:
         await ctx.channel.purge(limit=amount + 1)
@@ -143,6 +143,12 @@ async def _9clear(ctx, amount: int):
     else:
         embed = discord.Embed(title="Error: Permission Denied.", description="Your role must need manage messages to be enabled.", color=0xff0000)
         await ctx.send(embed=embed, delete_after=8)
+
+@bot.event #Erorr ignore for CommandNotFound
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        raise error
+        await ctx.send("Command Not Found")
 
 @bot.event #Error ignore for MissingPermissions
 async def on_command_error(ctx, error):
