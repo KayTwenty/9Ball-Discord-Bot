6b43eb3de8af99b2f11e5cc5d567af36afaf3534
K-20 <69999193+K-209@users.noreply.github.com>
2021-03-19 14:27:21 -0400

    Add files via upload

--- 9Ball.py
+++ 9Ball.py
@@ -4,6 +4,7 @@ import datetime
 import time
 import os
 import platform
+import sys
 from random import choice
 from discord.ext import commands, tasks
 from discord.ext.commands import MissingPermissions
@@ -19,7 +20,7 @@ bot.remove_command('help')
 async def status():
     while True:
         await bot.wait_until_ready()
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game("/9help"))
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game("/9help | /9b"))
         await sleep(3600)
         await bot.change_presence(status=discord.Status.online, activity=discord.Game("Version 2.0"))
         await sleep(500)
@@ -52,6 +53,7 @@ async def status():
 async def on_ready():
     users = len(set(bot.get_all_members()))
     ping = (time.monotonic()) / 10000
+    print('-----------------------------------------------------------------------------')
     print(str(datetime.datetime.now().time()) + " - Connecting to Discord API...")
     print(str(datetime.datetime.now().time()) + " - Connected to Discord API.")
     print(str(datetime.datetime.now().time()) + " - Loading stats and posting to DBL...")
@@ -61,6 +63,7 @@ async def on_ready():
     print('Servers connected to:')
     for guild in bot.guilds:
         print(guild.name)
+    print('------------------------------------------------------------------------------')
 bot.loop.create_task(status())
 
 @bot.command(aliases=['9ball', '9Ball', '9b', '9B', 'ball', 'Ball']) #The Main 9Ball command
@@ -96,11 +99,12 @@ async def _9ball(ctx, *, question):
                 "Definitely."]
 
     colors = [0x680af5,0x2E10ED,0x8CF9C1,0xF88000,0xFCFF00,0xed129f,0xed3212,0x1ACFE7,0x0FD150,0xFE2D00,]
-    embed=discord.Embed(title="The official 9Ball has Spoken.", color= random.choice(colors))
+    time = datetime.datetime.utcnow()
+    embed=discord.Embed(title="The official 9Ball has Spoken.", color=random.choice(colors))
     embed.set_author(name="Asked by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
     embed.add_field(name="Question:", value=question, inline=False)
     embed.add_field(name="Answer:", value=random.choice(responses), inline=False)
-    embed.set_footer(text="Commands: /9b | /9help")
+    embed.set_footer(text=time)
     await ctx.send(embed=embed)
     await ctx.message.delete()
 
@@ -120,7 +124,7 @@ async def _9help(ctx):
 @bot.command(aliases=['9about', '9About']) #The about page for 9Ball
 async def _9about(ctx):
     embed=discord.Embed(title="Developed By K-20", description="Written in Python Rewrite. It's not an 8Ball but, It's a 9Ball!", color=0x680af5)
-    embed.set_author(name="Requested by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
+    embed.set_author(name="Requested by: " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
     embed.set_thumbnail(url="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/ae/ae834ef83d652c19e5de5ec93a35c887a575517a_full.jpg")
     embed.add_field(name="Website:", value="https://sites.google.com/view/9-ball-bot/home", inline=False)
     await ctx.send(embed=embed)
@@ -147,31 +151,42 @@ async def _9clear(ctx, amount: int):
         embed = discord.Embed(title="Error: Permission Denied.", description="Your role must need manage messages to be enabled.", color=0xff0000)
         await ctx.send(embed=embed, delete_after=8)
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) #Cookie Command
 async def cookie(ctx, member: discord.Member):
     """Give a cookie to someone."""
     embed = discord.Embed(title="This person has gave you a cookie!", description="**{1}** gave a cookie to **{0}**! :cookie:".format(member.name, ctx.message.author.name), color=0x680af5)
+    embed.set_author(name="Cookie sent by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
+    embed.set_image(url="https://media1.tenor.com/images/45fe45f75ec523c2abf4e75ca2ac2fe2/tenor.gif?itemid=11797931")
+    embed.set_footer(text="Command: /cookie @user")
     await ctx.send(embed=embed)
+    await ctx.message.delete()
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) #Bonk Command
 async def bonk(ctx, member: discord.Member):
     """Shoot someone."""
     embed = discord.Embed(title="**Bonk!**", description="**{1}** Bonked **{0}**!".format(member.name, ctx.message.author.name), color=0x680af5)
-    embed.set_image(url="https://i.imgur.com/t1a9akh.gif")
+    embed.set_author(name="Bonked by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
+    embed.set_image(url="https://media1.tenor.com/images/0f145914d9e66a19829d7145daf9abcc/tenor.gif?itemid=19401897")
+    embed.set_footer(text="Command: /bonk @user")
     await ctx.send(embed=embed)
+    await ctx.message.delete()
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) #Hug Command
 async def hug(ctx, member: discord.Member):
     """Hug someone."""
     embed = discord.Embed(title="Sending...", description="**{1}** hugs **{0}**!".format(member.name, ctx.message.author.name), color=0x680af5)
+    embed.set_author(name="Hugged by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
     embed.set_image(url="https://media1.tenor.com/images/29a4aef07fde6e590aeaa3381324bbd1/tenor.gif?itemid=18630098")
+    embed.set_footer(text="Command: /hug @user")
     await ctx.send(embed=embed)
+    await ctx.message.delete()
 
-@bot.event #Erorr ignore for CommandNotFound
-async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        raise error
-        await ctx.send("Command Not Found")
+@bot.command(aliases=['quit', 'stop', 'exit']) #Shutdown command for Nineball
+async def shutdown(ctx):
+    await ctx.send("9Ball is shutting down. Please wait...", delete_after=10)
+    currentDT = self.mySupport.getTime()
+    print(f"[{currentDT}] 9Ball is shutting down...")
+    quit()
 
 @bot.event #Error ignore for MissingPermissions
 async def on_command_error(ctx, error):
