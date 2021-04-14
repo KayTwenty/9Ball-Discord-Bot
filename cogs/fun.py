c1b0c1216852c69a2dc3be10c119ee6d00ebe4f6
K-20 <69999193+K-209@users.noreply.github.com>
2021-04-14 08:36:00 -0400

    Add files via upload

--- cogs/fun.py
+++ cogs/fun.py
@@ -14,7 +14,7 @@ class fun(commands.Cog):
         embed = discord.Embed(title="This person has gave you a cookie!", description="**{1}** gave a cookie to **{0}**! :cookie:".format(member.name, ctx.message.author.name), color=0x680af5)
         embed.set_author(name="Cookie sent by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
         embed.set_image(url="https://media1.tenor.com/images/45fe45f75ec523c2abf4e75ca2ac2fe2/tenor.gif?itemid=11797931")
-        embed.set_footer(text="Command: /cookie @user")
+        embed.set_footer(text="Command: 9cookie @user")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
@@ -23,7 +23,7 @@ class fun(commands.Cog):
         embed = discord.Embed(title="This person has gave you a bunch of cookies", description="**{1}** gave cookies to **{0}**! :cookie:".format(member.name, ctx.message.author.name), color=0x680af5)
         embed.set_author(name="Cookies sent by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
         embed.set_image(url="https://media1.tenor.com/images/30c8ce96272fe73f58841164a179f6d1/tenor.gif?itemid=17729544")
-        embed.set_footer(text="Command: /cookies @user")
+        embed.set_footer(text="Command: 9cookies @user")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
@@ -33,7 +33,7 @@ class fun(commands.Cog):
         embed = discord.Embed(title="**Bonk!**", description="**{1}** Bonked **{0}**!".format(member.name, ctx.message.author.name), color=0x680af5)
         embed.set_author(name="Bonked by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
         embed.set_image(url="https://media1.tenor.com/images/0f145914d9e66a19829d7145daf9abcc/tenor.gif?itemid=19401897")
-        embed.set_footer(text="Command: /bonk @user")
+        embed.set_footer(text="Command: 9bonk @user")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
@@ -43,7 +43,7 @@ class fun(commands.Cog):
         embed = discord.Embed(title="Sending...", description="**{1}** hugs **{0}**!".format(member.name, ctx.message.author.name), color=0x680af5)
         embed.set_author(name="Hugged by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
         embed.set_image(url="https://media1.tenor.com/images/29a4aef07fde6e590aeaa3381324bbd1/tenor.gif?itemid=18630098")
-        embed.set_footer(text="Command: /hug @user")
+        embed.set_footer(text="Command: 9hug @user")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
@@ -52,7 +52,7 @@ class fun(commands.Cog):
         embed = discord.Embed(title="*This person gave you a suprise!*", description="**{1}** kissed **{0}**!".format(member.name, ctx.message.author.name), color=0x680af5)
         embed.set_author(name="Kissed by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
         embed.set_image(url="https://media1.tenor.com/images/4700f51c48d41104e541459743db42ae/tenor.gif?itemid=17947049")
-        embed.set_footer(text="Command: /kiss @user | (Covid Free)")
+        embed.set_footer(text="Command: 9kiss @user | (Covid Free)")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
@@ -61,7 +61,7 @@ class fun(commands.Cog):
         embed = discord.Embed(title="This person gave you a fistbump ;)", description="**{1}** fistbumped **{0}**!".format(member.name, ctx.message.author.name), color=0x680af5)
         embed.set_author(name="Fistbumped by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
         embed.set_image(url="https://media2.giphy.com/media/l0HlL6XHioKD5Gsgg/giphy.gif?cid=ecf05e473oo7yozme81o170s0i9tjwxdb7pq69ba46acewt0&rid=giphy.gif")
-        embed.set_footer(text="Command: /fistbump @user | /fb @user")
+        embed.set_footer(text="Command: 9fistbump @user | 9fb @user")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
@@ -70,7 +70,7 @@ class fun(commands.Cog):
         embed = discord.Embed(title="This person had sex with you ;)", description="**{1}** fucked **{0}**!".format(member.name, ctx.message.author.name), color=0x680af5)
         embed.set_author(name="Fucked by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
         embed.set_image(url="https://media1.tenor.com/images/fa98b23ca1dba1925da62f834f27153f/tenor.gif?itemid=19355212")
-        embed.set_footer(text="Command: /frick @user | /fuck @user")
+        embed.set_footer(text="Command: 9frick @user | 9fuck @user")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
@@ -79,7 +79,7 @@ class fun(commands.Cog):
         embed = discord.Embed(title="This person had fingered you ;)", description="**{1}** fingered **{0}**!".format(member.name, ctx.message.author.name), color=0x680af5)
         embed.set_author(name="Fingered by " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
         embed.set_image(url="https://media1.tenor.com/images/36dff1e9341831727baaac83a394e327/tenor.gif?itemid=19804695")
-        embed.set_footer(text="Command: /finger @user | /fin @user")
+        embed.set_footer(text="Command: 9finger @user | 9fin @user")
         await ctx.send(embed=embed)
         await ctx.message.delete()
 
