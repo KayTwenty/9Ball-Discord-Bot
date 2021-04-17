71a8123367548fad661999394e9cfec080cc64b3
K-20 <69999193+K-209@users.noreply.github.com>
2021-04-17 15:38:54 -0400

    Add files via upload

---
+++ cogs/shipping.py
@@ -0,0 +1,129 @@
+import discord, asyncio, random, json
+from discord.ext import commands
+from discord.ext.commands import clean_content
+
+class shipping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['gay-scanner', 'gayscanner', 'gay'])
+    async def gay_scanner(self, ctx,* ,member: discord.Member):
+        if not member:
+            member = ctx.author.name
+        gayness = random.randint(0,100)
+        if gayness <= 33:
+            gayStatus = random.choice(["No homo", 
+                                       "Wearing socks", 
+                                       '"Only sometimes"', 
+                                       "Straight-ish", 
+                                       "No homo bro", 
+                                       "Girl-kisser", 
+                                       "Hella straight"])
+            gayColor = 0xFFC0CB
+        elif 33 < gayness < 66:
+            gayStatus = random.choice(["Possible homo", 
+                                       "My gay-sensor is picking something up", 
+                                       "I can't tell if the socks are on or off", 
+                                       "Gay-ish", 
+                                       "Looking a bit homo", 
+                                       "lol half  g a y", 
+                                       "safely in between for now"])
+            gayColor = 0xFF69B4
+        else:
+            gayStatus = random.choice(["LOL YOU GAY XDDD FUNNY", 
+                                       "HOMO ALERT", 
+                                       "MY GAY-SENSOR IS OFF THE CHARTS", 
+                                       "STINKY GAY", 
+                                       "BIG GEAY", 
+                                       "THE SOCKS ARE OFF", 
+                                       "HELLA GAY"])
+            gayColor = 0xFF00FF
+        emb = discord.Embed(title="Gay-Scanner™", description=f"Gayness for **{member.mention}**", color=gayColor)
+        emb.add_field(name="Gayness:", value=f"{gayness}% gay")
+        emb.add_field(name="Comment:", value=f"{gayStatus} :kiss_mm:")
+        emb.set_author(name="Requested By " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
+        await ctx.send(embed=emb)
+        await ctx.message.delete()
+
+    @commands.command()
+    async def ship(self, ctx, member1: discord.Member, member2: discord.Member):
+        shipnumber = random.randint(0,100)
+        if 0 <= shipnumber <= 10:
+            status = "Really low! {}".format(random.choice(["Friendzone ;(", 
+                                                            'Just "friends"', 
+                                                            '"Friends"', 
+                                                            "Little to no love ;(", 
+                                                            "There's barely any love ;("]))
+        elif 10 < shipnumber <= 20:
+            status = "Low! {}".format(random.choice(["Still in the friendzone", 
+                                                     "Still in that friendzone ;(", 
+                                                     "There's not a lot of love there... ;("]))
+        elif 20 < shipnumber <= 30:
+            status = "Poor! {}".format(random.choice(["But there's a small sense of romance from one person!", 
+                                                     "But there's a small bit of love somewhere", 
+                                                     "I sense a small bit of love!", 
+                                                     "But someone has a bit of love for someone..."]))
+        elif 30 < shipnumber <= 40:
+            status = "Fair! {}".format(random.choice(["There's a bit of love there!", 
+                                                      "There is a bit of love there...", 
+                                                      "A small bit of love is in the air..."]))
+        elif 40 < shipnumber <= 60:
+            status = "Moderate! {}".format(random.choice(["But it's very one-sided OwO", 
+                                                          "It appears one sided!", 
+                                                          "There's some potential!", 
+                                                          "I sense a bit of potential!", 
+                                                          "There's a bit of romance going on here!", 
+                                                          "I feel like there's some romance progressing!", 
+                                                          "The love is getting there..."]))
+        elif 60 < shipnumber <= 70:
+            status = "Good! {}".format(random.choice(["I feel the romance progressing!", 
+                                                      "There's some love in the air!", 
+                                                      "I'm starting to feel some love!"]))
+        elif 70 < shipnumber <= 80:
+            status = "Great! {}".format(random.choice(["There is definitely love somewhere!", 
+                                                       "I can see the love is there! Somewhere...", 
+                                                       "I definitely can see that love is in the air"]))
+        elif 80 < shipnumber <= 90:
+            status = "Over average! {}".format(random.choice(["Love is in the air!", 
+                                                              "I can definitely feel the love", 
+                                                              "I feel the love! There's a sign of a match!", 
+                                                              "There's a sign of a match!", 
+                                                              "I sense a match!", 
+                                                              "A few things can be imporved to make this a match made in heaven!"]))
+        elif 90 < shipnumber <= 100:
+            status = "True love! {}".format(random.choice(["It's a match!", 
+                                                           "There's a match made in heaven!", 
+                                                           "It's definitely a match!", 
+                                                           "Love is truely in the air!", 
+                                                           "Love is most definitely in the air!"]))
+
+        if shipnumber <= 33:
+            shipColor = 0xE80303
+        elif 33 < shipnumber < 66:
+            shipColor = 0xff6600
+        else:
+            shipColor = 0x3be801
+
+        emb = (discord.Embed(color=shipColor, \
+                             title="Love test for:", \
+                             description="**{0}** and **{1}** {2}".format(member1, member2, random.choice([
+                                                                                                        ":sparkling_heart:", 
+                                                                                                        ":heart_decoration:", 
+                                                                                                        ":heart_exclamation:", 
+                                                                                                        ":heartbeat:", 
+                                                                                                        ":heartpulse:", 
+                                                                                                        ":hearts:", 
+                                                                                                        ":blue_heart:", 
+                                                                                                        ":green_heart:", 
+                                                                                                        ":purple_heart:", 
+                                                                                                        ":revolving_hearts:", 
+                                                                                                        ":yellow_heart:", 
+                                                                                                        ":two_hearts:"]))))
+        emb.add_field(name="Results:", value=f"{shipnumber}%", inline=True)
+        emb.add_field(name="Status:", value=(status), inline=False)
+        emb.set_author(name="Requested By " + str(ctx.message.author), icon_url=ctx.message.author.avatar_url)
+        await ctx.send(embed=emb)
+        await ctx.message.delete()
+
+def setup(bot):
+    bot.add_cog(shipping(bot))
