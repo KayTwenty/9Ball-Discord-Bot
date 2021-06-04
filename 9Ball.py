77e385cf730d27b46e3d5d4f87f83f66475c51f6
K-20 <69999193+K-209@users.noreply.github.com>
2021-06-04 15:17:07 -0400

    Add files via upload

--- 9Ball.py
+++ 9Ball.py
@@ -18,15 +18,15 @@ for filename in os.listdir('./cogs'):
 async def status():
     while True:
         await client.wait_until_ready()
-        await client.change_presence(status=discord.Status.online, activity=discord.Game("Version 2.3"))
+        await client.change_presence(status=discord.Status.online, activity=discord.Game("Happy Pride Month!!"))
+        await sleep(3600)
+        await client.change_presence(status=discord.Status.online, activity=discord.Game("Version 2.4"))
         await sleep(3600)
         await client.change_presence(status=discord.Status.online, activity=discord.Game("9help | 9b"))
         await sleep(3600)
-        await client.change_presence(status=discord.Status.online, activity=discord.Game("9gay | 9ship"))
+        await client.change_presence(activity=discord.Streaming(name="Smort Nerd", url="https://www.twitch.tv/sodapoppin"))
         await sleep(3600)
-        await client.change_presence(activity=discord.Streaming(name="WonUpped's Channel", url="http://www.twitch.tv/wonupped"))
-        await sleep(3600)
-        await client.change_presence(status=discord.Status.online, activity=discord.Game("Staying Alive"))
+        await client.change_presence(status=discord.Status.online, activity=discord.Game("9gif"))
         await sleep(3600)
         await client.change_presence(status=discord.Status.online, activity=discord.Game("9clear"))
         await sleep(3600)
@@ -44,7 +44,7 @@ async def status():
         await sleep(3600)
         await client.change_presence(status=discord.Status.online, activity=discord.Game("Raw Spaghetti Laser Beam!"))
         await sleep(3600)
-        await client.change_presence(status=discord.Status.online, activity=discord.Game("I Believe In The Sword."))
+        await client.change_presence(status=discord.Status.online, activity=discord.Game("Almost Summer"))
         await sleep(3600) #Status changer for the bot
 
 def ownercheck(ctx):
@@ -73,13 +73,6 @@ async def reload(ctx, extension):
     client.unload_extension(f"cogs.{extension}")
     client.load_extension(f"cogs.{extension}")
     await ctx.send("9Ball has reloaded the cog!")
-
-@commands.check(ownercheck)
-@client.command(aliases=['quit', 'stop', 'exit']) #Shutdown command for Nineball
-async def shutdown(ctx):
-    await ctx.send("9Ball is shutting down. Please wait...", delete_after=10)
-    print("9Ball is shutting down...")
-    await logout()
 
 @client.event #Error ignore for MissingPermissions
 async def on_command_error(ctx, error):
